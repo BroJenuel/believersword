bee665c662ec2a2e2c85a9a0b7c3b579ae8e37bb
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-03-18 19:05:42 +0800

    Initial Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    ssr: false,
+    devtools: { enabled: true },
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/color-mode",
+        "shadcn-nuxt",
+        "@vueuse/nuxt",
+        "nuxt-icon",
+    ],
+    css: ["@/src/index.css"],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: "",
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: "./components/ui",
+    },
+    colorMode: {
+        classSuffix: "",
+    },
+});
