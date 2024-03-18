8d2a054ac66603acd4f9b082799643ff2378a690
Jenuel Ganawed <jenuelganawed936@gmail.com>
2024-03-19 07:57:56 +0800

    created headers

--- components/bs/MenuList.vue
+++ components/bs/MenuList.vue
@@ -11,10 +11,10 @@ const props = defineProps({
                 v-bind="$attrs"
                 class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
             >
-                <div class="text-green12 mb-[5px] font-medium leading-[1.2]">
+                <div class="text-green-300 mb-[5px] font-medium leading-[1.2]">
                     {{ props.title }}
                 </div>
-                <p class="text-mauve11 my-0 leading-[1.4]">
+                <p class="text-sm my-0 leading-[1.4]">
                     <slot />
                 </p>
             </a>
